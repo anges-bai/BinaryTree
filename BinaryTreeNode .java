fd795fcc1ab94b4a8fdc30ed311b153f772f749c
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-01-25 00:13:54 +0800

    Update and rename 1.java to BinaryTreeNode .java

---
+++ BinaryTreeNode .java
@@ -0,0 +1,15 @@
+
+public class BinaryTreeNode {
+private Object data; 
+private BinaryTreeNode left; 
+private BinaryTreeNode right;
+
+}
+
+public class BinaryTreeNode {
+private Object data; 
+private BinaryTreeNode left; 
+private BinaryTreeNode right;
+private BinaryTreeNode parent; 
+
+}
